f8054057325811157f4e0c9e46be190c4a1bb119
harshgan9 <harsh.gandhipgdm2018@gmail.com>
2024-01-25 05:44:17 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -234,3 +234,9 @@ def setUpClass(cls):
     """Run once before all tests"""
     { other lines of code here ... }
     talisman.force_https = False
+    def test_cors_security(self):
+    """It should return a CORS header"""
+    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # Check for the CORS header
+    self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
